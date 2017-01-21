630ff9d114d8319bbf3038f88f128fc6ab2914d1
Jorge Wahnish <wahnish.jorge@gmail.com>
2017-01-21 12:53:44 -0300

    Camera effect + Standard Asset image effects

---
+++ WaveShooter/Assets/Sources/BloodBaby.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BloodBaby : MonoBehaviour
+{
+	private List<GameObject> _blood = new List<GameObject>();
+
+	void Awake()
+	{
+		foreach (Transform child in transform)
+		{
+			_blood.Add(child.gameObject);
+		}
+	}
+
+	public void Activate(Vector3 xPos)
+	{
+		gameObject.transform.position = xPos;
+		foreach (GameObject go in _blood)
+		{
+			go.gameObject.SetActive(true);
+		}
+		Destroy(this.gameObject,1.5f);
+	}
+}
---
+++ WaveShooter/Assets/Sources/BloodyCamera.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BloodyCamera : MonoBehaviour
+{
+	public Texture2D _tex;
+	public bool _activateBloody;
+	private bool _spit;
+	private Rect _r;
+	private float _bloodRate;
+	private float _time = 0.9f;
+
+	void Start()
+	{
+		_r = new Rect(0, 0, Screen.width, Screen.height);
+		ActivateBloody();
+	}
+
+	void Update()
+	{
+		
+		if (_activateBloody)
+		{
+			_bloodRate -= Time.deltaTime;
+			if (_bloodRate < 0)
+				_activateBloody = false;
+		}
+	}
+
+	public void ActivateBloody()
+	{
+		_activateBloody = true;
+		_bloodRate = _time;
+	}
+
+	void OnGUI()
+	{
+		if (_activateBloody)
+		{
+			GUI.DrawTexture(_r, _tex);
+		}
+	}
+}
